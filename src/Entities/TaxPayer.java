c96caf9929326399aa5714d79d87407e409887c2
Vitor <vitorvvc2005@gmail.com>
2023-11-14 14:30:51 -0300

    Atualizando classe TaxPayer

--- src/Entities/TaxPayer.java
+++ src/Entities/TaxPayer.java
@@ -1,9 +1,30 @@
 package Entities;
 
-public class TaxPayer {
+public abstract class TaxPayer {
 
-    private String name;
-    private Double anualIcome;
+    protected String name;
+    protected Double anualIcome;
 
+    public TaxPayer(String name, Double anualIcome) {
+        this.name = name;
+        this.anualIcome = anualIcome;
+    }
 
+    public abstract Double tax();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnualIcome() {
+        return anualIcome;
+    }
+
+    public void setAnualIcome(Double anualIcome) {
+        this.anualIcome = anualIcome;
+    }
 }
