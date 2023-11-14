7267affdeb673d675e9ff204b77d979754344963
Vitor <vitorvvc2005@gmail.com>
2023-11-14 14:32:52 -0300

    Push na classe Individual

--- src/Entities/Individual.java
+++ src/Entities/Individual.java
@@ -1,2 +1,38 @@
-package Entities;public class Individual {
+package Entities;
+
+public class Individual extends TaxPayer {
+
+    private Double healthExpenditures;
+
+    public Individual(String name, Double anualIcome, Double healthExpenditures) {
+        super(name, anualIcome);
+        this.healthExpenditures = healthExpenditures;
+    }
+    @Override
+    public Double tax() {
+        Double taxaTemp = null;
+        Double taxaFinal = null;
+
+        try {
+            if (anualIcome < 20000) {
+                taxaTemp = anualIcome * 0.15;
+            } else if (anualIcome >= 20000) {
+                taxaTemp = anualIcome * 0.25;
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException("Erro no método TAX Individual: "+exception.getMessage());
+        }
+        if (healthExpenditures != null) {
+            taxaFinal = taxaTemp - (healthExpenditures * 0.50);
+        }
+        return taxaFinal;
+    }
+
+    public Double getHealthExpenditures() {
+        return healthExpenditures;
+    }
+
+    public void setHealthExpenditures(Double healthExpenditures) {
+        this.healthExpenditures = healthExpenditures;
+    }
 }
